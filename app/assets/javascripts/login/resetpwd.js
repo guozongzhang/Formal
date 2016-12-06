299ced27f392efc76e2b454287e340448fbda6f5
yuguo6211 <yuguocug@sina.com>
2016-12-05 23:47:40 -0800

    找回密码

--- app/assets/javascripts/login/resetpwd.js
+++ app/assets/javascripts/login/resetpwd.js
@@ -2,12 +2,13 @@ let model
 var ip_host = 'http://192.168.1.120/openapi'
 // let ip_host =' http://123.57.217.65:3010';
 //验证码60秒倒计时
-let start_time = 99;//开始时间
+let start_time = 60;//开始时间
 class Resetpwd extends Basic {
   constructor(){
     super({
       vue: {
         data:{
+          step:'first',
           info:{
             number:'',
             picverify:'',
@@ -21,7 +22,7 @@ class Resetpwd extends Basic {
         }
       }
     })
-    this.register(['getVerify', 'confirmBtn', 'changeImg'])
+    this.register(['getVerify', 'confirmBtn', 'changeImg', 'NewPwdConfirm', 'getNext'])
     model = this
     this.init();
   }
@@ -39,6 +40,7 @@ class Resetpwd extends Basic {
       data:{
         width: 105,
         height:40,
+        font_size: 28
       },
       crossDomain: true,
       headers:{
@@ -59,6 +61,45 @@ class Resetpwd extends Basic {
     model.getImgCode();
   }
 
+  /*验证图形验证码、手机号是否注册*/
+  getNext(){
+    if(!(/^1(3|4|5|7|8)\d{9}$/.test($.trim(model.mvvm.info.number)))){ 
+      alert("手机号格式不正确"); 
+
+      return false; 
+    } 
+    if(_.isEmpty($.trim(model.mvvm.info.picverify))){ 
+      alert("验证码不能为空");  
+      return false; 
+    } 
+    $.ajax({
+      type:'post',
+      url: ip_host + '/api/1.0/functions/captcha/captcha',
+      data:{
+        mobile: model.mvvm.info.number,
+        code: model.mvvm.info.picverify,
+      },
+      crossDomain: true,
+      headers:{
+        "X-DP-Key": "0c31e550cfdab86f2c2ea59327907798",
+        "X-DP-ID": "cfdab86f2c2ea593"
+      },
+      success:function(msg) {
+        if(msg.status == 1) {
+          alert('该手机号还没有注册过，请输入正确的账号');
+          return ;
+        }
+        if(msg.status == 2) {
+          model.mvvm.step = 'second';
+        }
+        
+      },
+      error:function(msg) {
+        alert(msg.responseJSON.message);
+      }
+    })
+  }
+
   /*定时器*/
   countdowntime() {
     var time = setTimeout(model.countdowntime,1000); 
@@ -72,7 +113,6 @@ class Resetpwd extends Basic {
       start_time--;
     }
     $('#get_verify').text(start_time+'s后重发');
-    $('#get_verify').attr('disabled','true');
   }
 
   /*获取验证码*/
@@ -83,12 +123,12 @@ class Resetpwd extends Basic {
         mobile: phone,
         type: 'reg',
       };
-
+      $('#get_verify').attr('disabled','true');
       $.ajax({
         type:'get',
         url: ip_host + '/api/1.0/requestSmsCode/sms',
         data:{
-          type:'web',
+          type:'reset',
           mobile:phone,
         },
         crossDomain: true,
@@ -102,6 +142,7 @@ class Resetpwd extends Basic {
         },
         error:function(msg) {
           alert(msg.responseJSON.message);
+          $('#get_verify').removeAttr('disabled');
         }
       })
     } else {
@@ -110,11 +151,43 @@ class Resetpwd extends Basic {
     }
   }
 
-  /*登录*/
+  NewPwdConfirm(){
+    if($.trim(model.mvvm.info.newpwd).length < 6 || $.trim(model.mvvm.info.newpwd).length > 20 ){
+      alert("密码为6-20个字符")
+    }
+  }
+
+  /*提交*/
   confirmBtn() {
-    let data = {}
-    
-    console.log(data);
+    if(_.isEmpty($.trim(model.mvvm.info.verify))){ 
+      alert("验证码不能为空");  
+      return false; 
+    }
+    if(_.isEmpty($.trim(model.mvvm.info.newpwd))){ 
+      alert("新密码不能为空");  
+      return false; 
+    }
+    let setpwd = {
+      mobile: model.mvvm.info.number,
+      code: model.mvvm.info.verify,
+      password: model.mvvm.info.newpwd
+    }
+    $.ajax({
+      type:'put',
+      url: ip_host + '/api/1.0/users/reset_pasd',
+      data:setpwd,
+      crossDomain: true,
+      headers:{
+        "X-DP-Key": "0c31e550cfdab86f2c2ea59327907798",
+        "X-DP-ID": "cfdab86f2c2ea593"
+      },
+      success:function(msg) {
+        window.location.href = '/login/index'
+      },
+      error:function(msg) {
+        alert(msg.responseJSON.message);
+      }
+    })
   }
 }
 
