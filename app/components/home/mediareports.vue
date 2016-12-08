e6f982d3c4990b14acc1a32a94726076aad7a123
yuguo6211 <yuguocug@sina.com>
2016-12-07 19:57:42 -0800

    媒体报道图片

--- app/components/home/mediareports.vue
+++ app/components/home/mediareports.vue
@@ -30,28 +30,28 @@
       return {
         media:[
           {
-            img_url:'',
+            img_url:'../../assets/imgs/3.png',
             title:'搭配家获创新工场合伙人天使投资',
             content:'近日，全球软装资源整合者——COLLOCATION搭配家与创新工场合伙人签署投资协议，正式获取后者提供的天使投资。本次投资将被用于搭配家的品牌建设、品质提升、产品工艺升级、业务推广等各个领域',
             link_more:'http://jiaju.sina.com.cn/news/20160829/6175943404397330738.shtml',
             time:'08/29 2016'
           },
           {
-            img_url:'',
+            img_url:'../../assets/imgs/1.png',
             title:'黎明家具前总裁肖敏创立“搭配家”(图)',
             content:'他叫肖敏，创立搭配家，他想做的不再是一家企业的运营管理，而是帮助众多家具企业进行基于互联网技术的流程再造。肖敏表示，搭配家运作一年，如今估值已达1亿元。',
             link_more:'http://news.163.com/16/0512/00/BMQVL8V800014AED.html',
             time:'05/12 2016'
           },
           {
-            img_url:'',
+            img_url:'../../assets/imgs/2.png',
             title:'亚美特携手搭配家 传统家具互联网新玩法',
             content:'亚美特注意到客户购买形式的转变，按照搭配家的三个阶段，四大环节进行终端再造，保持传统的优势下，携手搭配家，重构成本，提高效率，线上揽客，线下体验，售后回访一站式服务。',
             link_more:'http://home.fang.com/news/2016-01-05/18937288.htm',
             time:'01/05 2016'
           },
           {
-            img_url:'',
+            img_url:'../../assets/imgs/4.png',
             title:'搭配家（京乐网络）当选北京家居协会副会长单位',
             content:'北京家居协会成立于1988年，是由北京市家具行业及相关的生产、经营、科研、教学等企业单位组成的家具专业性组织机构。会长是何法涧先生，协会成员有居然之家、集美、金隅天坛、曲美、利丰、强力、红星美凯龙、黎明和东方百盛等。',
             link_more:'http://www.bfta.org.cn/xhjs/fhzdw.asp',
