5c975eb6c6782d188e69ac63c897b4c6044469e3
yuguo6211 <yuguocug@sina.com>
2016-12-10 23:16:14 -0800

    上线配置示例

--- config/local_env.rule.js
+++ config/local_env.rule.js
@@ -1,6 +1,23 @@
 // This is the default site server config
 // if you want to change the config, we don't recommand to change this file 
 // the right way is create a file `local_env.json` and set you config
+// 
+// 
+// example:
+//   {
+//     "port":"5000",
+//     "Ips":{
+//         "home":"test.dpjia.com",
+//         "mall":"test_mall.dpjia.com",
+//         "design":"test_design.dpjia.com",
+//         "tools":"test_tools.dpjia.com",
+//         "services":"test_services.dpjia.com"
+//       },
+//       "app_key":"0c31e550cfdab86f2c2ea59327907798",
+//       "app_id":"cfdab86f2c2ea593",
+//       "url":"test_open.dpjia.com/api/1.0/"
+//   }
+ 
 
 
 module.exports = {
