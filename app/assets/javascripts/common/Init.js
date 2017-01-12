c0f844918c984cf8c159a232f60a827a585f25a6
yuguo6211 <yuguocug@sina.com>
2017-01-11 23:36:11 -0800

    test

--- app/assets/javascripts/common/Init.js
+++ app/assets/javascripts/common/Init.js
@@ -13,6 +13,7 @@ function getLoginState(){
     console.log('init',email,token)
     // 未登录进入个人中心页面
     if(!Cookies.get('dpjia') && SITE.router.controller === 'personal') {
+      console.log(_.isEmpty(email),_.isEmpty(token))
       if(!_.isEmpty(email) && !_.isEmpty(token)) {
         console.log('判断有token')
         //window.location.href = "/login/index?email=" + email + '&token=' + token;
