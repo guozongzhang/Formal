369b8bbc1a1d180daa5ce35c026c43c9c1e6cd42
yuguo6211 <yuguocug@sina.com>
2017-10-20 19:49:54 -0700

    message tips

--- app/assets/javascripts/common/API.js
+++ app/assets/javascripts/common/API.js
@@ -190,13 +190,68 @@ window.API = {
     }else{
       if (dataList) { dataList.show() }
     }
+  },
+
+  // 消息提醒
+  showNotification: (str, obj) => {
+    var NotificationHandler = {
+      isNotificationSupported: 'Notification' in window,
+      isPermissionGranted: function () {
+        return Notification.permission === 'granted'
+      },
+      requestPermission: function () {
+        if (!this.isNotificationSupported) {
+          console.log('the current browser does not support Notification API')
+          return
+        }
+        Notification.requestPermission(function (status) {
+          // status是授权状态，如果用户允许显示桌面通知，则status为'granted'
+          // permission只读属性
+          var permission = Notification.permission
+          // default 用户没有接收或拒绝授权 不能显示通知
+          // granted 用户接受授权 允许显示通知
+          // denied  用户拒绝授权 不允许显示通知
+        })
+      },
+      showNotifications: function () {
+        if (!this.isNotificationSupported) {
+          console.log('the current browser does not support Notification API')
+          return
+        }
+        if (!this.isPermissionGranted()) {
+          console.log('the current page has not been granted for notification')
+          return
+        }
+        var n = new Notification(obj.title, {
+          icon: 'http://cimg.dpjia.com/files/uploads/images/06652eacf0b66a1ed0e256ce8d22d3c1.png',
+          body: obj.content || ''
+        })
+        // onshow函数在消息框显示时会被调用
+        // 可以做一些数据记录及定时操作等
+        n.onshow = function () {
+          // 5秒后关闭消息框
+          setTimeout(function () {
+            n.close()
+          }, 5000)
+        }
+        // 消息框被点击时被调用
+        // 可以打开相关的视图，同时关闭该消息框等操作
+        n.onclick = function () {
+          n.close()
+        }
+        // 当有错误发生时会onerror函数会被调用
+        // 如果没有granted授权，创建Notification对象实例时，也会执行onerror函数
+        n.onerror = function () {
+        }
+        // 一个消息框关闭时onclose函数会被调用
+        n.onclose = function () {
+        }
+      }
+    }
+    if (str === 'test') {
+      NotificationHandler.requestPermission()
+    } else {
+      NotificationHandler.showNotifications()
+    }
   }
-
-
-
-
 }
-
-
-
-
