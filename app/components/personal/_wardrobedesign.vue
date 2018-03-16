a8321fce15f4f570b58efe5795f0ca9f0f836b0b
Razusa <skullkyle@hotmail.com>
2018-03-16 17:19:55 +0800

    修改了协议

--- app/components/personal/_wardrobedesign.vue
+++ app/components/personal/_wardrobedesign.vue
@@ -79,7 +79,7 @@
         let urlStr = (SITE.API.url).split('/api/')[0] + '/api'
         let token = Cookies.get('token-' + window.location.port)
         // hosturl=http://192.168.1.120/openapi/api&apiversion=/1.0/&appid=111&appkey=222&sessiontoken=b95ceea2b1224560134ef9218ac58bae&bureauid=543&isedit=true&pid=5310
-        window.location.href = 'DPBureau://hosturl=' + urlStr + '&apiversion=/1.0/' + '&appid=' + SITE.app_id + '&appkey=' + SITE.app_key + '&sessiontoken=' + token + '&bureauid=' + obj.id + '&isedit=true' + '&pid=' + obj.configuration_poi_product_configuration + '&configurationname=' + obj.name + '&productname=' + obj.name
+        window.location.href = 'DPBureau://hosturl=' + urlStr + '&apiversion=/1.0/' + '&appid=' + SITE.app_id + '&appkey=' + SITE.app_key + '&sessiontoken=' + token + '&bureauid=' + obj.id + '&isedit=true' + '&ispersonal=true' + '&pid=' + obj.configuration_poi_product_configuration + '&configurationname=' + obj.name + '&productname=' + obj.name
       },
       deletewardrobe: function(obj){
         tmp = obj;
